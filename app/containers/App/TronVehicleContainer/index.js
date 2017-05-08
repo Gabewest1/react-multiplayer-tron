2938a723dd8b38d87be9742f7ea9b871af61afc5
Gabriel West <gabe_west1@yahoo.com>
2017-05-07 21:49:44 -0500

    Added four `TronVehicle`s

--- app/containers/App/TronVehicleContainer/index.js
+++ app/containers/App/TronVehicleContainer/index.js
@@ -1,8 +1,15 @@
 import React from "react"
+import { bindActionCreators } from "redux"
+import { connect } from "react-redux"
+
+import * as actions from "./actions"
 
 import TronVehicle from "components/TronVehicle"
 
 export default class TronVehicleContainer extends React.Component {
+    constructor() {
+
+    }
     render() {
         return (
             <TronVehicle {...state} />
@@ -14,4 +21,8 @@ function mapStateToProps(state) {
     return {
         ...state.tronVehicle
     }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(actions, dispatch)
 }
