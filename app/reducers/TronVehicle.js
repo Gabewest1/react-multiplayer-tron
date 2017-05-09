6db009cb1e69cdfcf314f18fea988d563fb1aeb9
Gabriel West <gabe_west1@yahoo.com>
2017-05-09 15:03:34 -0500

    Setup reducer logic to handle updating the location on screen of a `TronVehicle`

--- app/reducers/TronVehicle.js
+++ app/reducers/TronVehicle.js
@@ -2,10 +2,10 @@ const velocity = 5
 
 let initialState = {
     tronVehicles: [
-        {tronVehicle1: {top: 0, left: 0, direction: "down", velocity: velocity, color: "red"}},
-        {tronVehicle2: {top: 0, left: 600, direction: "down", velocity: velocity, color: "green"}},
-        {tronVehicle3: {top: 600, left: 0, direction: "up", velocity: -velocity, color: "blue"}},
-        {tronVehicle4: {top: 600, left: 600, direction: "up", velocity: -velocity, color: "orange"}}
+        {tronVehicle0: {top: 0, left: 0, direction: "down", velocity: velocity, color: "red"}},
+        {tronVehicle1: {top: 0, left: 600, direction: "down", velocity: velocity, color: "green"}},
+        {tronVehicle2: {top: 600, left: 0, direction: "up", velocity: -velocity, color: "blue"}},
+        {tronVehicle3: {top: 600, left: 600, direction: "up", velocity: -velocity, color: "orange"}}
     ]
 }
 
@@ -13,16 +13,17 @@ export default function tronVehicle(state = initialState, action) {
     switch(action.type) {
         case "SET_VEHICLE_POSITION": {
             let { tronVehicle, position, value } = action
-            let tronVehicleState = state.tronVehicles.filter(vehicle => vehicle === tronVehicle)
-            let newState = {
-                ...state, 
-                tronVehicles: [
-                    ...state.tronVehicles,
-                    {...tronVehicle, [position]: value}
-                ]
-            }
-            console.log(newState)
-            return newState
+            let tronVehicleToUpdate = state.tronVehicles.filter(vehicle => vehicle.hasOwnProperty(tronVehicle))[0]
+            let updatedTronVehicle = {...tronVehicleToUpdate}
+            updatedTronVehicle[tronVehicle][position] = value   
+
+            let updatedTronVehicles = 
+                    state.tronVehicles.map(vehicle => vehicle.hasOwnProperty(tronVehicle) 
+                                                    ? updatedTronVehicle 
+                                                    : vehicle
+                                          )                         
+
+            return {...state, tronVehicles: updatedTronVehicles}
         }
         default: 
             return state
