8fbf816d99293a4fd6dc80fbf1b9314bb28eba14
Gabriel West <gabe_west1@yahoo.com>
2017-05-11 22:04:45 -0500

    Made TronGame width and height adjustable by props

--- app/containers/TronGame/Container.js
+++ app/containers/TronGame/Container.js
@@ -1,6 +1,9 @@
 import styled from "styled-components"
 
 let Container = styled.div`
+    border: solid 5px #fff033;
+    width: ${(props) => props.width && props.width}
+    height: ${(props) => props.height && props.height}
     position: relative;
 `
 
